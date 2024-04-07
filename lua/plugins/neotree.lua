2f343d07ce9b9103582086627fc080939ab43526
TheNofis <matbey122@gmail.com>
2024-04-07 03:17:46 +0000

    update

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,6 +5,24 @@ vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 require("neo-tree").setup({
 	window = {
+		position = "left",
 		width = 30,
+		mapping_options = {
+			noremap = true,
+			nowait = true,
+		},
+	},
+	filesystem = {
+		filtered_items = {
+			hide_dotfiles = false,
+			hide_gitignored = true,
+		},
+		follow_current_file = {
+			enabled = true,
+			leave_dirs_open = true,
+		},
+		group_empty_dirs = false,
+		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+		use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
 	},
 })
