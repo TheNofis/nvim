6afb54bdb85c1a18063b7647f7a22c7c881d2e5f
TheNofis <matbey122@gmail.com>
2024-10-04 10:41:31 +0300

    add git support

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,6 +4,7 @@ local map = vim.api.nvim_set_keymap
 
 -- NeoTree
 map("n", "<Leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
+map("n", "<Leader>r", ":Neotree source=git_status<CR>", { noremap = true, silent = true })
 
 -- Save / Exit
 map("n", "<c-s>", ":w<CR>", { noremap = true, silent = true })
@@ -21,6 +22,11 @@ map("n", "<c-left>", ":vertical resize +5<CR>", { noremap = true, silent = true 
 map("n", "<c-h>", ":wincmd h<CR>", { noremap = true, silent = true })
 map("n", "<c-l>", ":wincmd l<CR>", { noremap = true, silent = true })
 
+-- GIT
+map("n", "<Leader>gs", ":Telescope git_status<CR>", { noremap = true, silent = true })
+map("n", "<Leader>gc", ":Telescope git_commits<CR>", { noremap = true, silent = true })
+
+-- CODEIUM
 vim.keymap.set("i", "<Leader><Tab>", function()
 	return vim.fn["codeium#Accept"]()
 end, { expr = true, silent = true })
