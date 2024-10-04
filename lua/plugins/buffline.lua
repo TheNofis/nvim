a515d1910a53f3d19d639ef373c9eb96e0c53eda
TheNofis <matbey122@gmail.com>
2024-10-04 10:11:07 +0300

    new indicator

--- lua/plugins/buffline.lua
+++ lua/plugins/buffline.lua
@@ -1,19 +1,32 @@
 require("bufferline").setup({
 	options = {
+		color_icons = true,
 		buffer_close_icon = "✘",
 		mode = "buffers",
-		-- offsets = {
-		--     {
-		--         filetype = "neo-tree",
-		--         text = "File Explorer",
-		--         separator = true,
-		--         padding = 1
-		--     }
-		-- },
-		diagnostics = "nvim_lsp",
+
+		offsets = {
+			{
+				filetype = "neo-tree",
+				text = "File Explorer",
+				separator = true,
+				padding = 0,
+			},
+		},
+
 		indicator = {
-			icon = " ✔ ", -- this should be omitted if indicator style is not 'icon'
+			icon = "▋",
+			-- icon = " ✔ ", -- this should be omitted if indicator style is not 'icon'
 			style = "icon",
 		},
+		hover = {
+			enabled = true,
+			delay = 200,
+			reveal = { "close" },
+		},
+
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			return "(" .. count .. ")"
+		end,
 	},
 })
