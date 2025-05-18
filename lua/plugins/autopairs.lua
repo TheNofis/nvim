e9d62ffa771457f1b3467232548204b134934530
TheNofis <matbey122@gmail.com>
2025-05-19 00:41:11 +0300

    fix: autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,1 +1,15 @@
-require("nvim-autopairs").setup()
+local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
+
+npairs.setup({
+	check_ts = true,
+	enable_check_bracket_line = false,
+})
+
+npairs.remove_rule('"')
+
+npairs.add_rule(Rule('"', '"'):with_pair(function(opts)
+	local prev_char = opts.line:sub(opts.col - 1, opts.col - 1)
+	local next_char = opts.line:sub(opts.col, opts.col)
+	return prev_char ~= '"' and next_char ~= '"'
+end))
