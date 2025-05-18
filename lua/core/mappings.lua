7f8f5223cc663137785a8bb746d37dbe5b4978b6
TheNofis <matbey122@gmail.com>
2025-05-19 00:21:15 +0300

    remove: sage

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,7 +4,7 @@ local map = vim.api.nvim_set_keymap
 
 -- NeoTree
 map("n", "<Leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
-map("n", "<Leader>g", ":Neotree source=git_status<CR>", { noremap = true, silent = true })
+map("n", "<Leader>g", ":Neotree toggle source=git_status<CR>", { noremap = true, silent = true })
 
 -- Save / Exit
 map("n", "<c-s>", ":w<CR>", { noremap = true, silent = true })
@@ -17,18 +17,17 @@ map("n", "<Leader>x", ":bdelete<CR>:bNext<CR>", { noremap = true, silent = true 
 
 map("n", "<c-right>", ":vertical resize -5<CR>", { noremap = true, silent = true })
 map("n", "<c-left>", ":vertical resize +5<CR>", { noremap = true, silent = true })
+map("n", "<c-up>", ":resize -5<CR>", { noremap = true, silent = true })
+map("n", "<c-down>", ":resize +5<CR>", { noremap = true, silent = true })
 
 -- BUFFER SWITCH
 map("n", "<c-h>", ":wincmd h<CR>", { noremap = true, silent = true })
 map("n", "<c-l>", ":wincmd l<CR>", { noremap = true, silent = true })
 
 -- GIT
-map("n", "<Leader>gs", ":Telescope git_status<CR>", { noremap = true, silent = true })
-map("n", "<Leader>gc", ":Telescope git_commits<CR>", { noremap = true, silent = true })
+map("n", "<Leader>s", ":Telescope git_status<CR>", { noremap = true, silent = true })
+map("n", "<Leader>c", ":Telescope git_commits<CR>", { noremap = true, silent = true })
 
 map("n", "<Esc>", ":nohlsearch<Bar>:echo<CR>", { silent = true })
-
-map("n", "<Leader>s", ":Lspsaga hover_doc<CR>", { noremap = true, silent = true })
-map("n", "<Leader>d", ":Lspsaga outline<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
