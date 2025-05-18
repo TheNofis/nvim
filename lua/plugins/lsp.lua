d22dae37fca02d04d883f6174ca87a4853c69640
TheNofis <matbey122@gmail.com>
2025-05-18 23:23:59 +0300

    fix: word

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
--- Sntup language servers.
+-- Setup language servers.
 local lspconfig = require("lspconfig")
 
 lspconfig.pyright.setup({})
