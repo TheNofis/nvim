4f226b9c549a4a237781c5b7f43f3164c024a805
TheNofis <matbey122@gmail.com>
2025-05-19 00:45:04 +0300

    red: move git_status to left

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -4,82 +4,6 @@ vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSi
 vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-local function dirname(path)
-	return path:match("(.*/)")
-end
-
-local function basename(path)
-	return path:match(".*/([^/]+)$")
-end
-
-local function stem(path)
-	return basename(path):match("(.+)%..+$") or basename(path)
-end
-
-local function rel_path(from, to)
-	local from_dir = dirname(from)
-	local to_path = to
-
-	if to_path:sub(1, #from_dir) == from_dir then
-		local rel = to_path:sub(#from_dir + 1)
-		return rel:gsub("%.tsx?$", ""):gsub("%.jsx?$", ""):gsub("%.ts$", ""):gsub("%.js$", ""):gsub("\\", "/")
-	else
-		return to_path:gsub("%.tsx?$", ""):gsub("%.jsx?$", ""):gsub("%.ts$", ""):gsub("%.js$", ""):gsub("\\", "/")
-	end
-end
-
-local function rename_and_update_imports(state)
-	local node = state.tree:get_node()
-	local old_path = node:get_id()
-	local old_name = node.name
-
-	vim.ui.input({ prompt = "New Name: ", default = old_name }, function(new_name)
-		if not new_name or new_name == "" or new_name == old_name then
-			return
-		end
-
-		local old_dir = dirname(old_path)
-		local new_path = old_dir .. new_name
-
-		local ok, err = os.rename(old_path, new_path)
-		if not ok then
-			print("Ошибка переименования: " .. err)
-			return
-		end
-
-		vim.cmd("edit " .. new_path)
-
-		local old_buf = vim.fn.bufnr(old_path)
-		if old_buf ~= -1 then
-			vim.api.nvim_buf_delete(old_buf, { force = true })
-		end
-
-		local import_basename = stem(old_path)
-
-		local cwd = vim.loop.cwd()
-		local rg_cmd = string.format("rg -l '%s' %s", import_basename, cwd)
-
-		local handle = io.popen(rg_cmd)
-		local result = handle:read("*a")
-		handle:close()
-
-		for file in result:gmatch("[^\r\n]+") do
-			local old_rel = rel_path(file, old_path)
-			local new_rel = rel_path(file, new_path)
-
-			local function escape(str)
-				return str:gsub("([%.%-%/])", "\\%1")
-			end
-
-			local sed_old = escape(old_rel)
-			local sed_new = escape(new_rel)
-
-			local sed_cmd = string.format("sed -i 's/%s/%s/g' %s", sed_old, sed_new, file)
-			os.execute(sed_cmd)
-		end
-	end)
-end
-
 require("neo-tree").setup({
 	window = {
 		position = "left",
@@ -88,12 +12,6 @@ require("neo-tree").setup({
 			noremap = true,
 			nowait = true,
 		},
-		mappings = {
-			R = "rename_and_update_imports",
-		},
-	},
-	commands = {
-		rename_and_update_imports = rename_and_update_imports,
 	},
 	filesystem = {
 		filtered_items = {
@@ -102,7 +20,7 @@ require("neo-tree").setup({
 		},
 		follow_current_file = {
 			enabled = true,
-			leave_dirs_open = true,
+			leave_dirs_open = false,
 		},
 		group_empty_dirs = false,
 		hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -110,8 +28,12 @@ require("neo-tree").setup({
 	},
 	git_status = {
 		window = {
-			position = "right",
+			position = "left",
 			width = 30,
+			mapping_options = {
+				noremap = true,
+				nowait = true,
+			},
 			mappings = {
 				["A"] = "git_add_all",
 				["u"] = "git_unstage_file",
@@ -134,12 +56,10 @@ require("neo-tree").setup({
 			},
 		},
 		symbols = {
-			-- Change type
 			added = " ", -- or "✚", but this is redundant info if you use git_status_colors on the name
 			modified = " ", -- or "", but this is redundant info if you use git_status_colors on the name
 			deleted = "✖", -- this can only be used in the git_status source
 			renamed = "󰁕", -- this can only be used in the git_status source
-			-- Status type
 			untracked = "",
 			ignored = "",
 			unstaged = "☒",
