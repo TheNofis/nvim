0a5057bb1d48c1c05305d03222f55c2bf6f8b12f
TheNofis <matbey122@gmail.com>
2025-05-19 00:12:10 +0300

    ref: anycase search

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -35,6 +35,9 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.smartindent = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = false
+
 -- Navigation
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
