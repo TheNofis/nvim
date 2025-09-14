890e40fc74719a0fc42816118229380e115beb75
TheNofis <matbey122@gmail.com>
2025-09-14 07:51:06 +0300

    update: disable border

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,10 +1,22 @@
+local stages = require("notify.stages.fade")("bottom_up")
+
 require("notify").setup({
 	max_width = 60,
 	max_height = 10,
 
-	top_down = false,
-	position = "bottom_right",
+	render = "compact",
 
-	stages = "static",
-	timeout = 500,
+	stages = {
+		function(...)
+			local opts = stages[1](...)
+			if opts then
+				opts.border = "none"
+				opts.height = 1
+				opts.row = opts.row + 1
+			end
+			return opts
+		end,
+		unpack(stages, 2),
+	},
+	timeout = 1000,
 })
