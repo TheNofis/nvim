6b290223527cb863270a70e2287d82839f57fc36
TheNofis <matbey122@gmail.com>
2025-09-14 08:50:09 +0300

    add: better comments

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,40 +2,41 @@ vim.g.mapleader = " "
 
 local map = vim.api.nvim_set_keymap
 
--- NeoTree
+--[[ NeoTree ]]
 map("n", "<Leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
 map("n", "<Leader>g", ":Neotree toggle source=git_status<CR>", { noremap = true, silent = true })
 
--- Save / Exit
+--[[ Save / Exit ]]
 map("n", "<c-s>", ":w<CR>", { noremap = true, silent = true })
 map("n", "<c-q>", ":q<CR>", { noremap = true, silent = true })
 
--- TAB SWITCH
+--[[ Переключение табов и закрытие буфера ]]
 map("n", "<Tab>", "<Plug>(cokeline-focus-next)", { silent = true })
 map("n", "<s-Tab>", "<Plug>(cokeline-focus-prev)", { silent = true })
 map("n", "<Leader>x", ":bdelete<CR>:bNext<CR>", { noremap = true, silent = true })
 
+--[[ Изменение размера окон ]]
 map("n", "<c-right>", ":vertical resize -5<CR>", { noremap = true, silent = true })
 map("n", "<c-left>", ":vertical resize +5<CR>", { noremap = true, silent = true })
 map("n", "<c-up>", ":resize -5<CR>", { noremap = true, silent = true })
 map("n", "<c-down>", ":resize +5<CR>", { noremap = true, silent = true })
 
--- BUFFER SWITCH
-map("n", "<c-h>", "<c-w>h")
-map("n", "<c-l>", "<c-w>l")
-map("n", "<c-j>", "<c-w>j")
-map("n", "<c-k>", "<c-w>k")
+--[[ Перемещение между окнами ]]
+map("n", "<c-h>", ":wincmd h<CR>", { noremap = true, silent = true })
+map("n", "<c-l>", ":wincmd l<CR>", { noremap = true, silent = true })
 
--- GIT
-map("n", "<Leader>ss", ":Telescope git_status<CR>", { noremap = true, silent = true })
-map("n", "<Leader>c", ":Telescope git_commits<CR>", { noremap = true, silent = true })
+--[[ GIT ]]
+map("n", "<Leader>gs", ":Telescope git_status<CR>", { noremap = true, silent = true })
+map("n", "<Leader>gc", ":Telescope git_commits<CR>", { noremap = true, silent = true })
 
+--[[ Убираем подсветку поиска по Esc ]]
 map("n", "<Esc>", ":nohlsearch<Bar>:echo<CR>", { silent = true })
 
+--[[ LSP ]]
 map("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
 map("n", "<leader>rf", "<cmd>TSToolsRenameFile sync<CR>", { noremap = true, silent = true })
-
 map("n", "<leader>s", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 
+--[[ Удаление без сохранения в регистр ]]
 vim.keymap.set("n", "<c-x>", '"_dd')
 vim.keymap.set("v", "<c-x>", '"_dd')
