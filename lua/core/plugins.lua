fedd89aca053914a7cd0c542c9d6de9392da0ff5
TheNofis <matbey122@gmail.com>
2025-11-07 02:19:57 +0300

    add: tsx auto comment

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -136,7 +136,18 @@ require("lazy").setup({
 		end,
 	},
 
-	{ "numToStr/Comment.nvim", keys = { "<Leader>/", "<Leader>/" } },
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+		keys = { "<Leader>/", "<Leader>/" },
+	},
 
 	{
 		"willothy/nvim-cokeline",
